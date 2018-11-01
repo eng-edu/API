6015852cbacf13eb3c1cd1834348ff6d4e8e9270
eng-edu <eduardo.eng15@hotmail.com>
2018-11-01 16:10:07 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();   
+const mysql = require('mysql');
+const port = process.env.PORT || 3000
+const indexRoute = require('./routes/index')
+
+//configura conexao com banco
+exports.connection = mysql.createConnection({
+  host: '191.252.193.192',
+  port: '3306',
+  user: 'root',
+  password: '6code384',
+  database: 'BDARCO'
+});
+
+//carregando rotas
+app.use('/api', indexRoute);
+ 
+module.exports = app;
