262f30c54a598bf45bf054b1bba1f55daa8feec5
eng-edu <eduardo.eng15@hotmail.com>
2018-11-17 01:14:56 -0200

    up

--- controllers/manterEtapa.js
+++ controllers/manterEtapa.js
@@ -39,34 +39,43 @@ exports.put2 = ('/submeterEtapa/:ID/:RESUMO', (req, res) => {
 
 });
 
-exports.put3 = ('/aprovarEtapa/:ID/:PROX_ID', (req, res) => {
+exports.put3 = ('/aprovarEtapa/:ID/:PROX_ID/:ARCO_ID', (req, res) => {
 
     const ID = req.params.ID
     const PROX_ID = req.params.PROX_ID
+    const ARCO_ID = req.params.ARCO_ID
 
-    var sqlQry1 = `UPDATE ETAPA SET STATUS = 1 WHERE ID = '${ID}'`
-    var sqlQry2 = `UPDATE ETAPA SET STATUS = 4 WHERE ID = '${PROX_ID}'`
 
+    //falta o id do arco
+
+    var sqlQry1 = `UPDATE ETAPA SET STATUS = 1 WHERE ID = '${ID}' AND ARCO_ID = '${ARCO_ID}'`
+    var sqlQry2 = `UPDATE ETAPA SET STATUS = 4 WHERE ID = '${PROX_ID}' AND ARCO_ID = '${ARCO_ID}'`
+    var sqlQry3 = `CALL verificar_completo(${ARCO_ID})`
+  
     execute.executeSQL(sqlQry1, function (results) {
         if (results['affectedRows'] > 0) {
             mudarStatusPorx()
         } else {
             console.log(results);
         }
-        
+
     });
 
     function mudarStatusPorx() {
         execute.executeSQL(sqlQry2, function (results) {
+
             if (results['affectedRows'] > 0) {
                 res.status(200).send({ results });
             } else {
-                res.status(405).send(results);
+                res.status(200).send(results);
             }
             console.log(results);
         });
-
     }
+
+    execute.executeSQL(sqlQry3, function (results) {
+            console.log(results);
+    });
 
 });
 
--- routes/routerEtapa.js
+++ routes/routerEtapa.js
@@ -9,7 +9,7 @@ const manterEtapa = require('../controllers/manterEtapa')
 //rotas
 router.put('/salvarEtapa/:ID/:RESUMO', manterEtapa.put1)
 router.put('/submeterEtapa/:ID/:RESUMO', manterEtapa.put2)
-router.put('/aprovarEtapa/:ID/:PROX_ID', manterEtapa.put3)
+router.put('/aprovarEtapa/:ID/:PROX_ID/:ARCO_ID', manterEtapa.put3)
 router.put('/reprovarEtapa/:ID', manterEtapa.put4)
 router.get('/listarEtapasArco/:ARCO_ID', manterEtapa.get5)
 
