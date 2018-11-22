f300ca7dd2316bfac12513b1bf6226c06a422a7e
eng-edu <eduardo.eng15@hotmail.com>
2018-11-22 01:18:45 -0200

    up

--- controllers/manterDocumento.js
+++ controllers/manterDocumento.js
@@ -61,16 +61,37 @@ exports.put = ('/:ID/:NOME/:CAMINHO/:ETAPA_ID/:ETAPA_ARCO_ID', (req, res) => {
 });
 
 exports.delet = ('/apagarArquivosEtapa/:ID', (req, res) => {
-    var sqlQry = `DELETE FROM DOCUMENTO WHERE ID = '${req.params.ID}'`;
-    execute.executeSQL(sqlQry, function (results) {
+   
+    var sqlQry1 = `SELECT CAMINHO FROM BDARCO.DOCUMENTO WHERE ID = '${req.params.ID}'`;
+    var sqlQry2 = `DELETE FROM DOCUMENTO WHERE ID = '${req.params.ID}'`;
+  
+    execute.executeSQL(sqlQry1, function (results) {
+        if (results.length > 0) {
+        
+            //apaga local
+            var caminho = results[0].CAMINHO
+            var fs = require('fs')
+            fs.unlinkSync(caminho)
 
-        if (results['affectedRows'] > 0) {
-            res.status(200).send({ results });
-        } else {
-            res.status(405).send(results);
+
+
         }
-        console.log(results);
     });
+
+
+
+ execute.executeSQL(sqlQry2, function (results) {
+
+     if (results['affectedRows'] > 0) {
+         res.status(200).send({ results });
+     } else {
+         res.status(405).send(results);
+     }
+     console.log(results);
+ });
+
+
+  
 
 })
 
